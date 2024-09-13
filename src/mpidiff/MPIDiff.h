6ab00cca89932da1f29af2802f3e0c2717e1f7ec
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2024-09-13 13:26:37 -0700

    Make diffs a quiet no-op if MPIDiff is not initialized (#7)
    * Remove noisy output

--- src/mpidiff/MPIDiff.h
+++ src/mpidiff/MPIDiff.h
@@ -231,7 +231,6 @@ class MPIDiff {
                        BinaryPredicate predicate, TToString toString) {
          // Check that we are in a valid state
          if (!Initialized()) {
-            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!" << std::endl;
             return;
          }
 
@@ -336,7 +335,6 @@ class MPIDiff {
 
          // Check that we are in a valid state
          if (!Initialized()) {
-            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!" << std::endl;
             return hasDiffs;
          }
 
