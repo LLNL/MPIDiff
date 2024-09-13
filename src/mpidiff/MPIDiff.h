852912f19564dcb9262ddcf1b2af165733dcfee1
Alan Dayton <dayton8@llnl.gov>
2024-09-13 13:04:59 -0700

    Flush lines on error messages

--- src/mpidiff/MPIDiff.h
+++ src/mpidiff/MPIDiff.h
@@ -231,7 +231,7 @@ class MPIDiff {
                        BinaryPredicate predicate, TToString toString) {
          // Check that we are in a valid state
          if (!Initialized()) {
-            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!\n";
+            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!" << std::endl;
             return;
          }
 
@@ -334,7 +334,7 @@ class MPIDiff {
 
          // Check that we are in a valid state
          if (!Initialized()) {
-            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!\n";
+            std::cerr << "[MPIDiff] MPIDiff::Init must be called before MPIDiff::Diff. Unable to perform diffs!" << std::endl;
             return hasDiffs;
          }
 
